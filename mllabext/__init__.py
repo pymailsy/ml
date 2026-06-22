48958592ab42b9a7bb323a9661cc6e9ff64b2a0d
pymailsy <pymailproj@gmail.com>
2026-06-22 23:35:06 +0530

    Update __init__.py

--- mllabext/__init__.py
+++ mllabext/__init__.py
@@ -2,11 +2,10 @@ import requests
 
 BASE_URL = "https://raw.githubusercontent.com/pymailsy/ml/main"
 
-def get_code(n):
-    url = f"{BASE_URL}/{n}.txt"
-    r = requests.get(url, timeout=30)
-    r.raise_for_status()
-    return r.text
-
 def run(n):
-    print(get_code(n))
+    url = f"{BASE_URL}/{n}.txt"
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    print(response.text)
